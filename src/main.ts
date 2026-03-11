b78c95f79a4128a01ac30443f65f1e4463eacf7f
Xavier Marques <xaviermarques4f@gmail.com>
2026-03-11 22:47:41 +0100

    feat: enable PWA support in app bootstrap
    - Add PWA meta tags to index.html (manifest, theme-color, apple-mobile-web-app)
    - Register service worker in main.ts (production only)
    - Configure registerWhenStable strategy for optimal performance

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideHttpClient, HttpClient, withInterceptors } from '@angular/common/http';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { importProvidersFrom, LOCALE_ID } from '@angular/core';
+import { importProvidersFrom, LOCALE_ID, isDevMode } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeCa from '@angular/common/locales/ca';
 import { TranslateModule, TranslateLoader, provideTranslateService } from '@ngx-translate/core';
 import { TRANSLATE_HTTP_LOADER_CONFIG, TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -37,6 +38,10 @@ bootstrapApplication(AppComponent, {
         provide: TranslateLoader,
         useClass: TranslateHttpLoader
       }
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
 });
