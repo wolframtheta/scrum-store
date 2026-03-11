1d9b50513ba7344bd115b83149ff763e59f8db70
Xavier Marques <xaviermarques4f@gmail.com>
2026-03-11 22:47:48 +0100

    feat: integrate PWA install banner in app root
    Add PWA install banner component to app root template,
    displaying above ion-router-outlet for global visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from './core/services/auth.service';
+import { PwaInstallBannerComponent } from './core/components/pwa-install-banner/pwa-install-banner.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, PwaInstallBannerComponent],
 })
 export class AppComponent implements OnInit {
   constructor(
